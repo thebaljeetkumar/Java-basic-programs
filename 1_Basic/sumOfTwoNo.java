5ea9b94887a12f6fd5a4ac459dc5340215208886
thebaljeetkumar <baljeetkumar123@gmail.com>
2026-03-02 15:09:17 +0530

    Removed .class files and added gitignore

--- 1_Basic/sumOfTwoNo.java
+++ 1_Basic/sumOfTwoNo.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 public class sumOfTwoNo{
     public static void main() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first number: ");
+        try (Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter first number: ");
         int num1 = sc.nextInt();
         System.out.print("Enter second number: ");
         int num2 = sc.nextInt();
         int sum = num1 + num2;
         System.out.println("The sum is: " + sum);
         sc.close();
+        }
+        
     }
 }
