2646fa85e44d237fb6a8321def9775cc3c1a47f3
thebaljeetkumar <baljeetkumar123@gmail.com>
2026-03-31 09:57:01 +0530

    Swap without uning third variable

---
+++ 1_Basic/swap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a = sc.nextInt();
+        System.out.println("Enter b:");
+        int b = sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+    }
+}
