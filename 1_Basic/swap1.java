597b6f473a213dd24796b94e2479b8adf87692bd
thebaljeetkumar <baljeetkumar123@gmail.com>
2026-04-01 09:40:06 +0530

    swap with using third variable

---
+++ 1_Basic/swap1.java
@@ -0,0 +1,11 @@
+public class swap1 {
+    public static void main(String [] args){
+        int a = 10;
+        int b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
